2e48f33d3617e1d33a78ba247d10d31ad4fba677
Xamis <brian.labbie@gmail.com>
2011-08-29 03:37:27 +0000

    removed debugging code

--- code/cgame/cg_draw.c
+++ code/cgame/cg_draw.c
@@ -764,7 +764,7 @@ static void CG_DrawStatusBar( void ) {
         //weapon mode
        // value = ps->stats[STAT_MODE];
         mode = weapmodes_save.string[cg.predictedPlayerState.weapon];
-        CG_Printf("weapmodes_save.string[cg.predictedPlayerState.weapon] is %c\n",weapmodes_save.string[cg.predictedPlayerState.weapon]);
+        //CG_Printf("weapmodes_save.string[cg.predictedPlayerState.weapon] is %c\n",weapmodes_save.string[cg.predictedPlayerState.weapon]);
         //These weapons do not have modes, don't draw a mode
         if (!( cg.predictedPlayerState.weapon == WP_HE
                ||cg.predictedPlayerState.weapon == WP_SMOKE
